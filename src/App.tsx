413fc57d4a609475e6d8b2e5ec2aea59e00fe32e
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-02-15 15:04:43 -0800

    form error styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import clsx from 'clsx'
 import * as yup from 'yup'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FormikProps,
+  FormikValues,
+} from 'formik'
 
 const validationSchema = yup.object({
   name: yup.string().required("Can't be blank"),
@@ -70,117 +78,194 @@ function App() {
             console.log({ values, actions })
           }}
         >
-          <Form id='card-details' className='flex max-w-md flex-col gap-8'>
-            <div className='relative flex flex-col'>
-              <label
-                htmlFor='name'
-                className='pb-1 text-sm tracking-widest text-_very-dark-violet'
-              >
-                CARDHOLDER NAME
-              </label>
-              <Field
-                id='name'
-                name='name'
-                placeholder='e.g. Jane Appleseed'
-                className='rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
-              />
-              <ErrorMessage
-                name='name'
-                component='div'
-                className='absolute -bottom-5 text-xs text-_error-red'
-              />
-            </div>
-
-            <div className='relative flex flex-col'>
-              <label
-                htmlFor='card'
-                className='pb-1 text-sm tracking-widest text-_very-dark-violet'
-              >
-                CARD NUMBER
-              </label>
-              <Field
-                id='card'
-                name='card'
-                placeholder='e.g. 1234 5678 9123 0000'
-                className='rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
-              />
-              <ErrorMessage
-                name='card'
-                component='div'
-                className='absolute -bottom-5 text-xs text-_error-red'
-              />
-            </div>
-
-            <div className='flex items-end gap-4'>
-              <div>
-                <label
-                  id='expireDate'
-                  className='pb-1 text-sm tracking-widest text-_very-dark-violet'
-                >
-                  EXP. DATE (MM/YY)
-                </label>
-                <div className='flex gap-2'>
-                  <div className='relative flex flex-col'>
-                    <Field
-                      id='month'
-                      name='month'
-                      placeholder='MM'
-                      aria-labelledby='expireDate'
-                      size='4'
-                      className='rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
-                    />
-                    <ErrorMessage
-                      name='month'
-                      component='div'
-                      className='absolute -bottom-5 whitespace-nowrap text-xs text-_error-red'
-                    />
-                  </div>
-                  <div className='flex flex-col'>
-                    <Field
-                      id='year'
-                      name='year'
-                      placeholder='YY'
-                      aria-labelledby='expireDate'
-                      size='4'
-                      className='rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
-                    />
-                    <ErrorMessage
-                      name='year'
-                      component='div'
-                      className='absolute -bottom-5 whitespace-nowrap text-xs text-_error-red'
-                    />
-                  </div>
-                </div>
-              </div>
+          {(props: FormikProps<any>) => (
+            <Form id='card-details' className='flex max-w-md flex-col gap-8'>
               <div className='relative flex flex-col'>
                 <label
-                  htmlFor='cvc'
-                  className='translate-y-[2px] pb-1 text-sm tracking-widest text-_very-dark-violet'
+                  htmlFor='name'
+                  className='pb-2 text-sm tracking-widest text-_very-dark-violet'
                 >
-                  CVC
+                  CARDHOLDER NAME
                 </label>
-                <Field
-                  id='cvc'
-                  name='cvc'
-                  placeholder='e.g. 123'
-                  size='16'
-                  className='rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
-                />
+                <Field id='name' name='name'>
+                  {({
+                    field,
+                    form,
+                  }: {
+                    field: FormikValues
+                    form: FormikValues
+                  }) => (
+                    <input
+                      type='text'
+                      {...field}
+                      placeholder='e.g. Jane Appleseed'
+                      className={clsx(
+                        form.touched.name && form.errors.name
+                          ? 'border-_error-red'
+                          : 'border-_light-grayish-violet',
+                        'rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
+                      )}
+                    />
+                  )}
+                </Field>
                 <ErrorMessage
-                  name='cvc'
+                  name='name'
                   component='div'
                   className='absolute -bottom-5 text-xs text-_error-red'
                 />
               </div>
-            </div>
 
-            <button
-              type='submit'
-              className='mt-3 w-full rounded-lg bg-_very-dark-violet p-3 text-_light-grayish-violet'
-            >
-              Confirm
-            </button>
-          </Form>
+              <div className='relative flex flex-col'>
+                <label
+                  htmlFor='card'
+                  className='pb-2 text-sm tracking-widest text-_very-dark-violet'
+                >
+                  CARD NUMBER
+                </label>
+                <Field id='card' name='card'>
+                  {({
+                    field,
+                    form,
+                  }: {
+                    field: FormikValues
+                    form: FormikValues
+                  }) => (
+                    <input
+                      type='text'
+                      {...field}
+                      placeholder='e.g. 1234 5678 9123 0000'
+                      className={clsx(
+                        form.touched.card && form.errors.card
+                          ? 'border-_error-red'
+                          : 'border-_light-grayish-violet',
+                        'rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
+                      )}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage
+                  name='card'
+                  component='div'
+                  className='absolute -bottom-5 text-xs text-_error-red'
+                />
+              </div>
+
+              <div className='flex items-end gap-4'>
+                <div className='flex flex-col gap-2'>
+                  <label
+                    id='expireDate'
+                    className='text-sm tracking-widest text-_very-dark-violet'
+                  >
+                    EXP. DATE (MM/YY)
+                  </label>
+                  <div className='relative flex gap-2'>
+                    <div className='flex flex-col'>
+                      <Field id='month' name='month'>
+                        {({
+                          field,
+                          form,
+                        }: {
+                          field: FormikValues
+                          form: FormikValues
+                        }) => (
+                          <input
+                            {...field}
+                            type='text'
+                            size={4}
+                            aria-labelledby='expireDate'
+                            placeholder='MM'
+                            className={clsx(
+                              form.touched.month && form.errors.month
+                                ? 'border-_error-red'
+                                : 'border-_light-grayish-violet',
+                              'rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
+                            )}
+                          />
+                        )}
+                      </Field>
+                      <ErrorMessage
+                        name='month'
+                        component='div'
+                        className='absolute -bottom-5 left-0 whitespace-nowrap text-xs text-_error-red'
+                      />
+                    </div>
+                    <div className='flex flex-col'>
+                      <Field id='year' name='year'>
+                        {({
+                          field,
+                          form,
+                        }: {
+                          field: FormikValues
+                          form: FormikValues
+                        }) => (
+                          <input
+                            {...field}
+                            type='text'
+                            size={4}
+                            aria-labelledby='expireDate'
+                            placeholder='YY'
+                            className={clsx(
+                              form.touched.year && form.errors.year
+                                ? 'border-_error-red'
+                                : 'border-_light-grayish-violet',
+                              'rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
+                            )}
+                          />
+                        )}
+                      </Field>
+                      <ErrorMessage
+                        name='year'
+                        component='div'
+                        className='absolute -bottom-5 left-0 whitespace-nowrap text-xs text-_error-red'
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='relative flex flex-col'>
+                  <label
+                    htmlFor='cvc'
+                    className='pb-2 text-sm tracking-widest text-_very-dark-violet'
+                  >
+                    CVC
+                  </label>
+                  <Field id='cvc' name='cvc'>
+                    {({
+                      field,
+                      form,
+                    }: {
+                      field: FormikValues
+                      form: FormikValues
+                    }) => (
+                      <input
+                        {...field}
+                        type='text'
+                        size={16}
+                        placeholder='e.g. 123'
+                        className={clsx(
+                          form.touched.cvc && form.errors.cvc
+                            ? 'border-_error-red'
+                            : 'border-_light-grayish-violet',
+                          'rounded-lg border px-3 py-2 text-lg placeholder:text-_light-grayish-violet'
+                        )}
+                      />
+                    )}
+                  </Field>
+                  <ErrorMessage
+                    name='cvc'
+                    component='div'
+                    className='absolute -bottom-5 text-xs text-_error-red'
+                  />
+                </div>
+              </div>
+
+              <button
+                type='submit'
+                className='mt-3 w-full rounded-lg bg-_very-dark-violet p-3 text-_light-grayish-violet'
+              >
+                Confirm
+              </button>
+            </Form>
+          )}
         </Formik>
       </section>
     </main>
