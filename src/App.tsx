7766da03c0e898467c276e86c450c1894f98b86b
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-02-15 00:52:27 -0800

    form schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,117 @@
+import * as yup from 'yup'
+import { Formik, Form, Field } from 'formik'
+
+const validationSchema = yup.object({
+  name: yup.string().required("Can't be blank"),
+  card: yup
+    .string()
+    .matches(/^[0-9]{16}$/, 'Wrong format, must be 16 digits')
+    .required("Can't be blank"),
+  month: yup.number().max(2).required("Can't be blank"),
+  year: yup.number().max(2).required("Can't be blank"),
+  cvc: yup.string().matches(/^[0-9]{3}$/, 'Wrong format, must be 3 digits'),
+})
+
+interface TFormValues extends yup.InferType<typeof validationSchema> {
+  name: string
+  card: string
+  month: number
+  year: number
+  cvc: string
+}
+
 function App() {
+  const initialValues = {
+    name: '',
+    card: '',
+    month: '',
+    year: '',
+    cvc: '',
+  }
+
   return (
     <main>
-      <div>0000 0000 0000 0000 Jane Appleseed 00/00</div>
-      <div>000</div>
-      <div>
-        <div>Cardholder Name e.g. Jane Appleseed</div>
+      {/* example card */}
+      <section>
+        {/* front of card */}
+        <div>
+          <div>
+            <span>0000</span>
+            <span>0000</span>
+            <span>0000</span>
+            <span>0000</span>
+          </div>
+          <div>
+            <div>Jane Appleseed</div>
+            <div>00/00</div>
+          </div>
+        </div>
+        {/* back of card */}
+        <div>
+          <div>000</div>
+        </div>
+      </section>
 
-        <div>Card Number e.g. 1234 5678 9123 0000</div>
-
-        <div>Exp. Date (MM/YY) MM YY</div>
-        <div>CVC e.g. 123</div>
-        <button>Confirm</button>
-      </div>
-
-      {/* Completed state start */}
-      <div>
-        Thank you! We've added your card details
-        <button>Continue</button>
-      </div>
-
-      <div>
-        Challenge by{' '}
-        <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
-          Frontend Mentor
-        </a>
-        . Coded by <a href='#'>Your Name Here</a>.
-      </div>
+      {/* form */}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions })
+        }}
+      >
+        <Form id='card-details'>
+          <label htmlFor='name'>
+            Cardholder Name
+            <Field id='name' name='name' placeholder='e.g. Jane Appleseed' />
+          </label>
+          <label htmlFor='card'>
+            <Field
+              id='card'
+              name='card'
+              placeholder='e.g. 1234 5678 9123 0000'
+            />
+          </label>
+          <label htmlFor='expire'>
+            <Field id='expire' name='expire' />
+          </label>
+          <fieldset form='card-details'>
+            <legend>Exp. Date (MM/YY)</legend>
+            <Field id='month' name='month' placeholder='MM' />
+            <Field id='year' name='year' placeholder='YY' />
+          </fieldset>
+          <label htmlFor='cvc'>
+            <Field id='cvc' name='cvc' placeholder='e.g. 123' />
+          </label>
+          <button type='submit'>Confirm</button>
+        </Form>
+      </Formik>
     </main>
+  )
+}
+
+// Completed State
+const Success = () => {
+  return (
+    <div>
+      Thank you! We've added your card details
+      <button>Continue</button>
+    </div>
+  )
+}
+
+const Attribution = () => {
+  return (
+    <div>
+      Challenge by{' '}
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a href='https://www.frontendmentor.io/profile/orphandeity'>
+        Jeff R Williams
+      </a>
+      .
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,42 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'bg-main-dektop': "url('/assets/bg-main-desktop.png')",
+        'bg-main-mobile': "url('/assets/bg-main-mobile.png')",
+        'bg-card-front': "url('/assets/bg-card-front.png')",
+        'bg-card-back': "url('/assets/bg-card-back.png')",
+      },
       colors: {
         '_gradient-1': '#6448fe',
         '_gradient-2': '#600594',
-        '_red': '#ff5252',
+        '_error-red': '#ff5252',
         '_light-grayish-violet': '#dedddf',
         '_dark-grayish-violet': '#8e8593',
         '_very-dark-violet': '#21092f',
       },
       fontFamily: {
-        'sans': ['"Space Grotesk"', 'sans-serif']
+        sans: ["'Space Grotesk'", 'sans-serif'],
       },
       screens: {
-        'mobile': '375px',
-        'desktop': '1440px'
-      }
+        mobile: '375px',
+        desktop: '1440px',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
 }
-
-
-// ## Colors
-
-// ### Primary
-
-// - Linear gradient (active input border): hsl(249, 99%, 64%) to hsl(278, 94%, 30%)
-// - Red (input errors): hsl(0, 100%, 66%)
-
-// ### Neutral
-
-// - White: hsl(0, 0%, 100%)
-// - Light grayish violet: hsl(270, 3%, 87%)
-// - Dark grayish violet: hsl(279, 6%, 55%)
-// - Very dark violet: hsl(278, 68%, 11%)
